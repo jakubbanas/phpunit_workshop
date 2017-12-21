669c622b4754818b8acf1f3091c2a1c86d615452
Jakub Banas <jakub.banas@kinguin.net>
2017-12-21 14:56:12 +0100

    unit tests

--- app/tests/TestTest.php
+++ app/tests/TestTest.php
@@ -1,16 +1,24 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use App\Model\{ Foo, Bar };
 
 final class TestTest extends TestCase
 {
-    public function testSuccess(): void
+    public function testBar(): void
     {
-        $this->assertTrue(true);
+        $this->expectOutputString('test bar');
+        print (new Bar())->test();
     }
 
-    public function testFail(): void
+    public function testFoo(): void
     {
-        $this->assertTrue(false);
+        $this->expectOutputString('whatever');
+
+        $bar = $this->createMock(Bar::class);
+        $bar->method('test')
+            ->willReturn("whatever");
+            
+        new Foo($bar);
     }
 }
